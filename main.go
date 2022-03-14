8fb4392e792f8321846c3f0abde775742575defe
Colin Toft <CToft@flexera.com>
2022-03-14 10:31:20 +0000

    Initial go commit

---
+++ main.go
@@ -0,0 +1,49 @@
+package main
+
+import "fmt"
+
+func main() {
+	conferenceName := "Flexera Conference"
+	const conferenceTickets int = 50
+	var remainingTickets uint = 50 
+	var bookings [50]string 
+
+	fmt.Printf("conferenceTickets is %T, remainingTickets is %T, conferenceName is %T\n", conferenceTickets, remainingTickets, conferenceName)
+
+	fmt.Printf("Welcome to our booking application for the %v.\n", conferenceName )
+	fmt.Printf("We have a total of %v tickets and %v are still available.\n", conferenceTickets, remainingTickets)
+	fmt.Println("Get your tickets here to attend the", conferenceName, "in June 2022!")
+
+	var firstName string
+	var lastName string
+	var email string
+	var userTickets uint
+
+	fmt.Println("Enter your first name: ")
+	fmt.Scan(&firstName)
+	// &var give us the pointer value of where the var is stored so we can reference it. 
+
+	fmt.Println("Enter your last name: ")
+	fmt.Scan(&lastName)
+
+	fmt.Println("Enter your email address: ")
+	fmt.Scan(&email)
+
+	fmt.Println("Enter number of tickets: ")
+	fmt.Scan(&userTickets)
+
+	remainingTickets = remainingTickets - userTickets
+	bookings [0] = firstName + " " + lastName 
+
+	fmt.Printf("The whole array: %v\n", bookings)
+	fmt.Printf("The first value: %v\n", bookings [0])
+	fmt.Printf("Array type: %T\n", bookings)
+	fmt.Printf("Array length: %v\n", len(bookings))
+
+
+
+
+
+	fmt.Printf("Thank you %v %v for booking %v tickets. You will recieve a confimation email to %v\n", firstName, lastName, userTickets, email)
+	fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
+}
